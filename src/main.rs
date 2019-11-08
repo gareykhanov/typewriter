91609651dfb3eb809126e8e1cdb5ea94fa7552cd
gareykhanov <gareykhanov@i-teco.ru>
2019-11-08 17:26:42 +0300

    improved print function, crossterm dependency

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
-use std::io::{BufRead};
-use std::fs::{File};
+use std::fs::File;
+use std::io::BufRead;
 
 struct ScreenInfo {
     width: usize,
-    height: usize
+    height: usize,
 }
 
 #[derive(Debug)]
-struct ScreenLine {
+struct PrintedLine {
     length: usize,
     row: usize,
-    nextLineCh: char
+    next_line_char: char,
 }
-impl ScreenLine {
-    fn new (row: usize, length: usize, nextLineCh: char) -> ScreenLine {
-        ScreenLine{ length, row, nextLineCh }
+impl PrintedLine {
+    fn new(row: usize, length: usize, next_line_char: char) -> PrintedLine {
+        PrintedLine {
+            length,
+            row,
+            next_line_char,
+        }
     }
 }
 
+struct PrintInfo {
+    lines: Vec<PrintedLine>,
+    next_ch_i: Option<usize>
+}
+impl PrintInfo {
+    fn new(lines: Vec<PrintedLine>, next_ch_i: Option<usize>) -> Self {
+        PrintInfo{lines, next_ch_i}
+    }
+}
 
 fn main() {
-    let screen_info = ScreenInfo { width: 130, height: 24 };
+    let screen_info = ScreenInfo {
+        width: 130,
+        height: 36,
+    };
     let chars = read_file("test/test.txt").unwrap();
-    let lines = print(&chars, &screen_info);
+    let info = print(&chars, &screen_info);
     println!();
-    for l in lines.iter() { 
-        println!("{:?}", l);
-    }
-    
+    //for l in lines.iter() {
+    //    println!("{:?}", l);
+    //}
 }
 
 fn read_file(path: &str) -> Result<Vec<char>, String> {
@@ -35,68 +50,65 @@ fn read_file(path: &str) -> Result<Vec<char>, String> {
     Ok(file_str.chars().collect())
 }
 
-fn print(print_buf: &Vec<char>, screen_info: &ScreenInfo) -> Vec<ScreenLine> {
-    
+fn print(print_buf: &Vec<char>, screen_info: &ScreenInfo) -> PrintInfo {
     //word
     let mut w_i = None;
     let mut w_len = 0;
     //word delimeter
     let mut del = '\0';
     let mut del_len = 0;
-    
+
     let mut line_len = 0;
 
     let mut lines = Vec::new();
     let mut row = 0;
-    
-    for (i, &c) in print_buf.iter().enumerate() {
 
-        if c != '\n' && c != ' ' { 
+    for (i, &c) in print_buf.iter().enumerate() {
+        if c != '\n' && c != ' ' {
             if w_i == None {
                 w_i = Some(i);
             }
             w_len += 1;
         } else {
-            if w_len > 0 {
-                let line_with_w = line_len + w_len + del_len;
-                let (on_new_line) = if del == '\n' {
-                    (true)
-                } else if line_with_w > screen_info.width {
-                    (true)
-                } else if line_with_w <= screen_info.width {
-                    (false)
-                } else {
-                    unreachable!();
-                };
-                if on_new_line {
-                    lines.push(ScreenLine::new(row, line_len, del));
-                    row += 1;
-                    line_len = w_len;
-                    print!("\n");
-                } else {
-                    line_len += w_len + del_len;
-                    if del != '\0' {
-                        print!("{}", del);
-                    }
+            let line_with_w = line_len + w_len + del_len;
+            let on_new_line = if del == '\n' {
+                (true)
+            } else if line_with_w > screen_info.width {
+                (true)
+            } else if line_with_w <= screen_info.width {
+                (false)
+            } else {
+                unreachable!();
+            };
+            if on_new_line {
+                lines.push(PrintedLine::new(row, line_len, del));
+                if row == screen_info.height {
+                    return PrintInfo::new(lines, Some(i));
                 }
+                row += 1;
+                line_len = w_len;
+                print!("\n");
+            } else {
+                line_len += w_len + del_len;
+                if del != '\0' {
+                    print!("{}", del);
+                }
+            }
+            if w_len != 0 {
                 for c in print_buf[w_i.unwrap()..w_i.unwrap() + w_len].iter() {
                     print!("{}", c);
                 }
-                if i == print_buf.len() - 1 {
-                    lines.push(ScreenLine::new(row, line_len, '\0'));
-                }
-                //print!("{},{}", w_len, line_len);
-                w_i = None;
-                w_len = 0;
-                del = c;
-                del_len = if c == '\n' { 0 } else { 1 };
             }
+            if i == print_buf.len() - 1 {
+                lines.push(PrintedLine::new(row, line_len, '\0'));
+            }
+            //print!("{},{}", w_len, line_len);
+            w_i = None;
+            w_len = 0;
+            del = c;
+            del_len = if c == '\n' { 0 } else { 1 };
         }
-        
     }
-    
-    lines 
+
+    PrintInfo::new(lines, None)
 }
-
-
-
